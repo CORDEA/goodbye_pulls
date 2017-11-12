408be2068420b0ecddb75ba9a70d4b9567930051
Yoshihiro Tanaka <contact@cordea.jp>
2017-11-12 12:10:02 +0900

    add error handling

--- lib/goodbye_pulls_web/controllers/controller.ex
+++ lib/goodbye_pulls_web/controllers/controller.ex
@@ -14,16 +14,42 @@ defmodule GoodbyePullsWeb.Controller do
     if is_opened params do
       pr = parse_request params
       url = gist_raw_url Application.get_env(:goodbye_pulls, :gist_id)
-      resp = HTTPoison.get! url
-      IO.inspect comment(pr, resp.body)
-      IO.inspect close_pull_request(pr)
-      render(conn, "pulls.json", [])
+      resp = HTTPoison.get url
+      handle_gist_response(conn, pr, resp)
     else
-      conn
-        |> put_status(500)
-        |> render(ErrorView, "500.json", [])
+      server_error conn
     end
   end
+
+  defp server_error(conn) do
+    conn
+      |> put_status(500)
+      |> render(ErrorView, "500.json", [])
+  end
+
+  defp handle_gist_response(conn, pr, {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    if succeed_comment? comment(pr, body) do
+      if succeed_close? close_pull_request(pr) do
+        render(conn, "pulls.json", [])
+      else
+        server_error conn
+      end
+    else
+      server_error conn
+    end
+  end
+
+  defp handle_gist_response(conn, _pr, {:ok, _response}), do: server_error conn
+
+  defp handle_gist_response(conn, _pr, {:error, _error}), do: server_error conn
+
+  defp succeed_comment?({code, _body}) when code == 200 or code == 201, do: true
+
+  defp succeed_comment?(_), do: false
+
+  defp succeed_close?({_code, _body}), do: false
+
+  defp succeed_close?(_), do: true
 
   defp is_opened(params) do
     params["action"] == "opened"
